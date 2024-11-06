05adb79ecc0c4204e1a3967d6c052f14683cec5f
Sid-Taha <twahasiddqui11@gmail.com>
2024-11-07 00:30:27 +0500

    first commit

---
+++ src/constant/card.ts
@@ -0,0 +1,50 @@
+export const cardData = [
+  {
+    id: 1,
+    backgoundColor : "#7EB5B8",
+    src : "/myImage/card1.png",
+    heading : "Regular fit long sleeve suit",
+    price : "$38.99",
+    rating : "5.0",
+  },
+  {
+    id: 2,
+    backgoundColor : "#90CDCC",
+    src : "/myImage/card2.png",
+    heading : "Regular fit long sleeve suit",
+    price : "$38.99",
+    rating : "5.0",
+  },
+  {
+    id: 3,
+    backgoundColor : "#A2E5E0",
+    src : "/myImage/card3.png",
+    heading : "Regular fit long sleeve suit",
+    price : "$38.99",
+    rating : "5.0",
+  },
+  {
+    id: 4,
+    backgoundColor : "#7EB5B8",
+    src : "/myImage/card4.png",
+    heading : "Regular fit long sleeve suit",
+    price : "$38.99",
+    rating : "5.0",
+  },
+  {
+    id: 5,
+    backgoundColor : "#90CDCC",
+    src : "/myImage/card5.png",
+    heading : "Regular fit long sleeve suit",
+    price : "$38.99",
+    rating : "5.0",
+  },
+  {
+    id: 6,
+    backgoundColor : "#A2E5E0",
+    src : "/myImage/card6.png",
+    heading : "Regular fit long sleeve suit",
+    price : "$38.99",
+    rating : "5.0",
+  }
+]
